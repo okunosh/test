2126fc915dffe54ba40b5f484172eb5c17e3be37
okunosh <uyosin0kuo1@gmail.com>
2024-09-14 13:10:13 +0900

    create blogs1

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,5 @@ Route::get('/', function () {
 */
 
 Route::get('/', [PostController::class, 'index']);
+Route::get('/posts/create', [PostController::class, 'create']);
 Route::get('/posts/{post}', [PostController::class ,'show']);
